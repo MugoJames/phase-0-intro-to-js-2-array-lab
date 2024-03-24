ae6d3e542652614bf4b27e66debacbdbaa6e00d0
James <jamonjoroge816@gmail.com>
2024-03-24 15:47:29 +0300

    array lab

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Write your solution here!
+// Write your solution here!
+
+let cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+  cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+  cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat(name) {
+  cats.pop(name);
+}
+
+function destructivelyRemoveFirstCat(name) {
+  cats.shift(name);
+}
+
+function appendCat(name) {
+    let newCatsArray = cats.concat(name);
+    return newCatsArray;
+}
+
+function  prependCat(name){
+    let newCatsArray = cats.concat(name);
+    return newCatsArray;
+}
+
+function prependCat(name) {
+    let newCatsArray = [name, ...cats];
+    return newCatsArray;
+}
+
+function removeLastCat(name){
+    let newCatsArray = cats.slice(0, -1);
+    return newCatsArray;
+}
+
+function removeFirstCat(name){
+    let newCatsArray = cats.slice(1);
+    return newCatsArray;
+}
